4c2910261599e4886f1e5510afbe1ba9f492aff0
Miudodo <miaodd1988@gmail.com>
2016-02-25 11:40:17 +0800

    Update sendjoke.py

--- sendjoke.py
+++ sendjoke.py
@@ -1,7 +1,7 @@
 #-*- coding:utf-8 -*-
 __author__ = 'miudodo'
 
-import requests,re
+import requests,re,random
 
 def get_content():
     url = 'http://apis.baidu.com/showapi_open_bus/showapi_joke/joke_text?page=1'
@@ -19,7 +19,8 @@ def get_content():
         # 只取第一条笑话正文
         #for t in text:
         #    print t
-        return u'笑话：'+ text[0]
+        i = random.randint(0,len(text) - 1)
+        return u'笑话：'+ text[i]
     else:
         return "系统错误，无笑话"
 
@@ -28,24 +29,31 @@ def sendsms():
     url_2 = 'https://wapmail.10086.cn/index.htm'
 
     #首页，获取JSESSIONID
+    headers = {
+        "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Encoding":"gzip, deflate, sdch",
+        "Accept-Language":"zh-CN,zh;q=0.8",
+        "Cache-Control":"no-cache",
+        "Connection":"keep-alive",
+        "Cookie":"",
+        "Host":"wapmail.10086.cn",
+        "Origin":"http://wapmail.10086.cn",
+        "Referer":"",
+        "Upgrade-Insecure-Requests":1,
+        "User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1",
+    }
+
     s = requests.session()
     cookie = s.get(url_1).cookies._cookies
     p = re.compile(r'JSESSIONID...value=.(.*?).,')
     jsessionid = p.findall(str(cookie))[0]
     Cookie = "%s=%s" % ("JSESSIONID",jsessionid)
-
-    headers = {
-        "Cookie":Cookie,
-        "Host":"wapmail.10086.cn",
-        "Origin":"http://wapmail.10086.cn",
-        "Referer":url_1,
-        "Upgrade-Insecure-Requests":1,
-        "User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1",
-    }
+    headers["Referer"] = url_1
+    headers["Cookie"] = Cookie
 
     data = {
-        "ur":"***********",  #139邮箱账号
-        "pw":"********",  #139邮箱密码
+        "ur":"13928960513",
+        "pw":"q387647979",
         "_fv":2,
         "submitInput":u"登录"
     }
@@ -54,6 +62,7 @@ def sendsms():
     r = s.post(url_2, data = data, headers = headers, allow_redirects=False)
     p2 = re.compile(r'\'location\': \'(.*?)\'')
     p3 = re.compile(r'sid=(.*?)&')
+    #print r.headers
     location =  p2.findall(str(r.headers))[0]
     sid = p3.findall(location)[0]
     cookies = str(r.cookies)
@@ -66,10 +75,10 @@ def sendsms():
 
 
     #发送短信
-    url_sms = 'http://wapmail.10086.cn/ws12/n2/send.htm?cmd=40&sid=%s' % sid
+    url_sms = 'http://wapmail.10086.cn/ws12/n2/send.htm?sid=%s' % sid
     url_send = 'http://wapmail.10086.cn/ws12/n2/send.htm'
 
-    phone = '********' #接收短信的号码
+    phone = '13928960513,13580518268,15202002820'
 
     headers["Referer"] = url_sms
     headers["Cookie"] = Cookie
@@ -79,7 +88,7 @@ def sendsms():
     data_sms = {
         "sid":sid,
         "cmd":2,
-        "rnum":012462,
+        #"rnum":012462,
         "l":1,
         "c":'',
         "s":'',
@@ -92,7 +101,7 @@ def sendsms():
 
     r_sms = s.post(url_send, data = data_sms, headers = headers)
     #print headers
-    #print data_sms
+    print content
     print r_sms.status_code
 
 if __name__ == '__main__':
